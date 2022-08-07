47f76346f74be1645dbaa299bc77a6b688306035
Pedro <pedro.miguel.lopes951@gmail.com>
2022-08-07 18:34:20 +0100

    Second Push

---
+++ OOP_Template.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using std::string;
+
+class Game
+{
+private:
+    string name;
+    string character;
+    int Level;
+    int health;
+public:
+    Game(/* args */);
+    ~Game();
+};
+
+Game::Game(/* args */)
+{
+}
+
+Game::~Game()
+{
+}
+
+int main()
+{
+}
