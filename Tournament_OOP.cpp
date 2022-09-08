bc2eb1cf5bae08434e97c7af227d39748b4f4d92
Pedro Lopes <pedro@LAPTOP-2BVQGF6K>
2022-09-08 20:23:46 +0100

    First Version completed

--- Tournament_OOP.cpp
+++ Tournament_OOP.cpp
@@ -6,26 +6,13 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <algorithm>
+#include <random>
 using std::string;
 using std::vector;
-class Tournament
-{
-private:
-    /* data */
-public:
-    Tournament(/* args */);
-    ~Tournament();
-};
-
-Tournament::Tournament(/* args */)
-{
-}
-
-Tournament::~Tournament()
-{
-}
-
-class Players : public Tournament
+class Players;
+class Tournament;
+class Players 
 {
 protected:
 private:
@@ -87,6 +74,276 @@ Players::Players(string name, string belt, int age, int weight)
 }
 
 Players::~Players()
+{
+}
+class Tournament 
+{
+private:
+  std::multimap<string, string> Adult_White;
+  std::multimap<string, string> Adult_Blue;
+  std::multimap<string, string> Adult_Purple;
+  std::multimap<string, string> Adult_Brown;
+  std::multimap<string, string> Adult_Black;
+  std::multimap<string, string> Master_White;
+  std::multimap<string, string> Master_Blue;
+  std::multimap<string, string> Master_Purple;
+  std::multimap<string, string> Master_Brown;
+  std::multimap<string, string> Master_Black;
+
+public:
+    Tournament(/* args */);
+    ~Tournament();
+    void Organize_Divisions(vector<Players*>);
+    void Show_Division();
+};
+void Tournament::Organize_Divisions(vector<Players*> players_list)
+{
+        // Iterate over all the players vector
+            // For each iteration see info of players and insert him on the correct struct
+        for (auto &it : players_list )
+        {
+            if (it->getBelt().find("White") != string::npos )
+            {
+                if (it->getAge() >= 18 && it->getAge() <= 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Adult_White.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Adult_White.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Adult_White.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Adult_White.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+                if ( it->getAge() > 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Master_White.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Master_White.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Master_White.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Master_White.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+            }
+            if (it->getBelt().find("Blue") != string::npos)
+            {
+                if (it->getAge() >= 18 && it->getAge() <= 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Adult_Blue.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Adult_Blue.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Adult_Blue.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Adult_Blue.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+                if ( it->getAge() > 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Master_Blue.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Master_Blue.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Master_Blue.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Master_Blue.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+            }
+            if (it->getBelt().find("Purple") != string::npos)
+            {
+                if (it->getAge() >= 18 && it->getAge() <= 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Adult_Purple.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Adult_Purple.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Adult_Purple.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Adult_Purple.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+                if ( it->getAge() > 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Master_Purple.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Master_Purple.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Master_Purple.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Master_Purple.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+            }
+            if (it->getBelt().find("Brown")!= string::npos )
+            {
+
+                if (it->getAge() >= 18 && it->getAge() <= 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Adult_Brown.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Adult_Brown.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Adult_Brown.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Adult_Brown.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+                if ( it->getAge() > 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Master_Brown.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Master_Brown.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Master_Brown.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Master_Brown.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+            }
+            if (it->getBelt().find("Black")!= string::npos )
+            {
+
+                if (it->getAge() >= 18 && it->getAge() <= 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Adult_Black.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Adult_Black.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Adult_Black.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Adult_Black.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+                if ( it->getAge() > 40)
+                {
+                    if (it->getWeight() <= 70)
+                    {
+                        Master_Black.insert(std::pair<string,string>("FeatherWeight",it->getName()));
+                    }
+                    if (70 < it->getWeight() && it->getWeight() <= 80)
+                    {
+                        Master_Black.insert(std::pair<string,string>("LightWeight",it->getName()));
+                    }
+                    if (80 < it->getWeight() && it->getWeight() <= 90)
+                    {
+                        Master_Black.insert(std::pair<string,string>("MiddleWeight",it->getName()));
+                    }
+                    if ( it->getWeight() > 90 )
+                    {
+                        Master_Black.insert(std::pair<string,string>("Heavyweight",it->getName()));
+                    }
+                }
+            }
+        }
+        
+    }
+
+void Tournament::Show_Division(){
+    // Ask for User to ask for which division he wants to see
+    while (1)
+    {
+    string division;
+    std::cout << "What Division do you want to see ('exit' to end) ? \n";
+    std::cin >> division;
+    if (division == "Blue")
+    {   
+        string category;
+        std::cout << "What Category do you want to see (FeatherWeight,LightWeight,MiddleWeight,Heavyweight) ? \n";
+        std::cin >> category;
+        std::cout << std::endl;
+        for (auto &itr : Adult_Blue) {
+            if (itr.first.find( category) != string::npos)
+            {
+                std::cout << '\t' << itr.second << '\n';
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Not found, try again" << '\n';
+    }
+    }
+}
+Tournament::Tournament(/* args */)
+{
+}
+
+Tournament::~Tournament()
 {
 }
 
--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
-/* Problem: Build a Tournament bracket using classes proporties. It should ask the user to input the characteristic of each athelete, organize the
-matches by Division (age,weight and belt) and build a bracket system that displays the bracket at the terminal and ask to display the Division.
+/* Goal: Build a Tournament bracket using classes proporties. 
+   Requirements:- It read txt file for the characteristic of each athelete
+                - Organize the matches by Division (age,weight and belt) 
+                - Build a bracket system that displays the bracket at the terminal
+                - Ask to disply the Division Bracket
 
 Conditions:
     No more than 6 players for now
 
 Future aplication :
-    - Search data from file 
     - Use a postgres database, instead of hard typing every thing
     - Given the winner the braket should update the division bracket and display the new matches, until a final winner is presented
 
 
 Implement :
-    Implement class tournament to build the tournament
-    Read from input file the data for participants
-    Use template for each entrie 
-    Create Unit testing to make sure 
+    - Implement class tournament to build the tournament
+    - Read from input file the data for participants
+    - Use template for each entrie 
+    - Create Unit testing to test the code 
 */
 #include "Tournament_OOP.cpp"
 
 /* Function Declaration- Function prototypes declaring each functino used in program*/
 vector<Players *> entry_of_atheletes();
-void present_playes(vector<Players*>);
+void present_players(vector<Players*>);
 int main()
 {
     // Vector that stores the participants which go into the tournament
@@ -28,5 +30,12 @@ int main()
     vector<Players *> players_list;
     players_list = entry_of_atheletes();
     present_players(players_list);  
+    //Create object Tournament
+    Tournament ADCC;
+    //Method consumes PLayers Vector and organizes based on belt, weight and age 
+    ADCC.Organize_Divisions(players_list);
+    ADCC.Show_Division();
+    // Ask for user to write which division he wants to check up
+    ADCC.Show_Division();
     return 0;
 }
